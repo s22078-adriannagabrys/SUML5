2140e7d1c1385efbcb8058d1b357444a1e81925d
Adrianna Gabryś <adrianna@kodanu.pl>
2024-05-29 16:57:29 +0200

    init2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import wikipediaapi
 import pickle
 import numpy as np
 from datetime import datetime
@@ -15,16 +16,36 @@ sex_d = {0: "Kobieta", 1: "Mężczyzna"}
 pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
 embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
 
+# Initialize the Wikipedia API
+wiki_wiki = wikipediaapi.Wikipedia('en')
+
+def fetch_wikipedia_page(title):
+    page = wiki_wiki.page(title)
+    if page.exists():
+        return page.summary
+    else:
+        return "The page does not exist."
+
 def main():
     st.set_page_config(page_title="Czy przeżyłbyś katastrofę?")
+
+    # Sidebar for Wikipedia search
+    st.sidebar.title("Wikipedia Search")
+    search_term = st.sidebar.text_input("Enter a search term")
+
+    if st.sidebar.button("Search"):
+        if search_term:
+            summary = fetch_wikipedia_page(search_term)
+            st.sidebar.write(summary)
+        else:
+            st.sidebar.write("Please enter a search term.")
+
     overview = st.container()
     left, right = st.columns(2)
     prediction = st.container()
 
     with overview:
         st.title("Czy przeżyłbyś katastrofę?")
-        st.image(
-            "https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
 
     with left:
         sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
@@ -32,14 +53,14 @@ def main():
         embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2, format_func=lambda x: embarked_d[x])
 
     with right:
-        age_slider = st.slider("Wiek", value=50, min_value=1, max_value=100)
-        sibsp_slider = st.slider("# Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
-        parch_slider = st.slider("# Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
-        fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=10)
+        age_input = st.text_input("Wiek", value="50")
+        sibsp_input = st.text_input("# Liczba rodzeństwa i/lub partnera", value="0")
+        parch_input = st.text_input("# Liczba rodziców i/lub dzieci", value="0")
+        fare_input = st.text_input("Cena biletu", value="0")
 
     if st.button("Predict"):
         # Prepare input data for prediction
-        data = np.array([[pclass_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio, sex_radio,]])
+        data = np.array([[int(pclass_radio), int(sex_radio), int(age_input), int(sibsp_input), int(parch_input), float(fare_input), int(embarked_radio)]])
 
         # Predict survival
         survival = model.predict(data)
@@ -49,7 +70,8 @@ def main():
             st.header(f"Czy dana osoba przeżyje? {'Tak' if survival[0] == 1 else 'Nie'}")
             st.subheader(f"Pewność predykcji {s_confidence[0][survival[0]] * 100:.2f} %")
 
-
+    # Move the image to the bottom
+    st.image("https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
 
 if __name__ == "__main__":
     main()
