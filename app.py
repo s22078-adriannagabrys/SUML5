d52bf851fda5c8e028be558e6763fb94be2d4cfd
Adrianna Gabryś <adrianna@kodanu.pl>
2024-05-29 16:15:37 +0200

    init2

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def main():
         embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2, format_func=lambda x: embarked_d[x])
 
     with right:
-        age_slider = st.slider("Wiek", value=50, min_value=1, max_value=100)
+        age_slider = st.slider("Wiek", value=38, min_value=1, max_value=100)
         sibsp_slider = st.slider("# Liczba rodzeństwa i/lub partnera", min_value=1, max_value=8)
         parch_slider = st.slider("# Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
         fare_slider = st.slider("Cena biletu", min_value=78, max_value=500, step=10)
