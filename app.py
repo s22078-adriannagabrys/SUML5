7f74a43b97aaa9afc1a85bf3e8a41133d08f8b67
Adrianna Gabryś <adrianna@kodanu.pl>
2024-05-29 17:04:41 +0200

    init2

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ sex_d = {0: "Kobieta", 1: "Mężczyzna"}
 pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
 embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
 
-# Initialize the Wikipedia API
-wiki_wiki = wikipediaapi.Wikipedia('en')
+# Initialize the Wikipedia API with a user agent
+wiki_wiki = wikipediaapi.Wikipedia('en', headers={'User-Agent': 'Tytanic/1.0 (s22078@pjwstk.edu.pl)'})
 
 def fetch_wikipedia_page(title):
     page = wiki_wiki.page(title)
