1296759761dba8e348aae331b0d25cae793fd992
Adrianna Gabryś <adrianna@kodanu.pl>
2024-05-29 18:46:38 +0200

    init2

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ with open(filename, 'rb') as file:
 sex_d = {0: "Kobieta", 1: "Mężczyzna"}
 pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
 embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
+
+
 def main():
     st.set_page_config(page_title="Czy przeżyłbyś katastrofę?")
-
     overview = st.container()
     left, right = st.columns(2)
     prediction = st.container()
@@ -25,9 +26,10 @@ def main():
         st.title("Czy przeżyłbyś katastrofę?")
 
     with left:
-        sex_radio = st.radio("Płeć", {0: "Kobieta", 1: "Mężczyzna"})
-        pclass_radio = st.radio("Klasa", {0: "Pierwsza", 1: "Druga", 2: "Trzecia"})
-        embarked_radio = st.radio("Port zaokrętowania", {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"})
+        sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
+        pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
+        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2,
+                                  format_func=lambda x: embarked_d[x])
 
     with right:
         age_slider = st.slider("Wiek", value=50, min_value=1, max_value=100)
@@ -37,12 +39,8 @@ def main():
 
     if st.button("Predict"):
         # Prepare input data for prediction
-        data = np.array([[pclass_radio, sex_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]])
-
-        # Load the pre-trained model
-        filename = "model.h5"
-        with open(filename, 'rb') as file:
-            model = pickle.load(file)
+        data = np.array(
+            [[pclass_radio, sex_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]])
 
         # Predict survival
         survival = model.predict(data)
@@ -52,7 +50,11 @@ def main():
             st.header(f"Czy dana osoba przeżyje? {'Tak' if survival[0] == 1 else 'Nie'}")
             st.subheader(f"Pewność predykcji {s_confidence[0][survival[0]] * 100:.2f} %")
 
-    st.image("https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
+    st.image(
+        "https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG")
+
 
 if __name__ == "__main__":
     main()
+
+
