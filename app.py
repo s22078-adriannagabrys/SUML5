dc968e97ebafdba23a6e780d7175635b563b45b5
Adrianna Gabryś <adrianna@kodanu.pl>
2024-05-29 16:18:43 +0200

    init2

--- app.py
+++ app.py
@@ -29,24 +29,25 @@ def main():
     with left:
         sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
         pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
-        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=0, format_func=lambda x: embarked_d[x])
+        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2, format_func=lambda x: embarked_d[x])
 
     with right:
-        age_slider = st.slider("Wiek", value=38, min_value=1, max_value=100)
-        sibsp_slider = st.slider("# Liczba rodzeństwa i/lub partnera", min_value=1, max_value=8)
+        age_slider = st.slider("Wiek", value=50, min_value=1, max_value=100)
+        sibsp_slider = st.slider("# Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
         parch_slider = st.slider("# Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
-        fare_slider = st.slider("Cena biletu", min_value=78, max_value=500, step=10)
+        fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=10)
 
-    # Prepare input data for prediction
-    data = np.array([[pclass_radio, sex_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]])
+    if st.button("Predict"):
+        # Prepare input data for prediction
+        data = np.array([[pclass_radio, sex_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]])
 
-    # Predict survival
-    survival = model.predict(data)
-    s_confidence = model.predict_proba(data)
+        # Predict survival
+        survival = model.predict(data)
+        s_confidence = model.predict_proba(data)
 
-    with prediction:
-        st.header(f"Czy dana osoba przeżyje? {'Tak' if survival[0] == 1 else 'Nie'}")
-        st.subheader(f"Pewność predykcji {s_confidence[0][survival[0]] * 100:.2f} %")
+        with prediction:
+            st.header(f"Czy dana osoba przeżyje? {'Tak' if survival[0] == 1 else 'Nie'}")
+            st.subheader(f"Pewność predykcji {s_confidence[0][survival[0]] * 100:.2f} %")
 
 if __name__ == "__main__":
     main()
